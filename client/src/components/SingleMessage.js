929641bd9fadafb323ecfe2df45bc2d4d0f2e69d
MacMania Networks, LLC <mail@celloben.com>
2023-02-06 19:30:04 -0500

    Fix routing for production.

--- client/src/components/SingleMessage.js
+++ client/src/components/SingleMessage.js
@@ -11,7 +11,7 @@ export default function SingleMessage() {
 
     useEffect(() => {
         async function getSingleEntry(id) {
-            const res = await axios.get(`http://localhost:4000/api/v1/entries/message/${id}`, {
+            const res = await axios.get(`api/v1/entries/message/${id}`, {
                 validateStatus: (status) => {
                     return status <= 500;
                 }
