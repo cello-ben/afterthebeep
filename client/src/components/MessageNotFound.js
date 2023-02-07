af85e96f304e6cb17e3fb195c997b12966773b32
MacMania Networks, LLC <mail@celloben.com>
2023-02-06 19:27:24 -0500

    Rebuild with layout tweaks and button to go back home from error pasge.

--- client/src/components/MessageNotFound.js
+++ client/src/components/MessageNotFound.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 export default function MessageNotFound() {
     const location = useLocation();
     const err = location.state;
     console.log(err);
     return (
-        <div className = "ml-4">
-        { err !== undefined && err !== null ? <h1 className = "jumbotron">{ err }</h1> : 'No error found.' }
-       
+        <div className = "jumbotron">
+        { err !== undefined && err !== null ? <h1 className = "">{ err }</h1> : 'No error found.' }
+        <Link to = ".." className = "btn btn-primary">Back Home</Link>
        </div>
     );
 };
